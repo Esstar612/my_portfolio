14f45bf3ebdd083b909ca965347094284d2e1e07
Staria Olaojo <97202548+Esstar612@users.noreply.github.com>
2024-11-11 22:42:39 -0500

    added icons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaMoon, FaSun, FaHome, FaCode, FaProjectDiagram, FaUser } from 'react-icons/fa';
 
 const Navbar = () => {
     const [darkTheme, setDarkTheme] = useState(false);
@@ -21,10 +21,26 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/open-source">Open Source</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/about">About Me</Link></li>
+                <li>
+                    <Link to="/">
+                        <FaHome /> Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/open-source">
+                        <FaCode /> Open Source
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/projects">
+                        <FaProjectDiagram /> Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        <FaUser /> About Me
+                    </Link>
+                </li>
             </ul>
             <button className="theme-toggle" onClick={toggleTheme}>
                 {darkTheme ? <FaMoon /> : <FaSun />}
